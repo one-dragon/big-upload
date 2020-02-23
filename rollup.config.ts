3f5b49d3a657456a2a76d68c2f48593e50b4e4d1
sun.yilong <sun.yilong@neusoft.com>
2020-02-23 12:15:46 +0800

    [build] 0.0.1

--- rollup.config.ts
+++ rollup.config.ts
@@ -7,7 +7,7 @@ import json from 'rollup-plugin-json'
 
 const pkg = require('./package.json')
 
-const libraryName = 'ts-axios'
+const libraryName = 'big-upload'
 
 export default {
   input: `src/index.ts`,
